c561a2d45db8d2d10c085754c87f792a489b0c9b
TisanaKit <htrae.back@gmail.com>
2016-03-09 15:43:37 +0700

    Add character shooting and enemy dead

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	public float speed = 10;
+	private float startTime;
+	public int damage = 10;
+
+	void Start()
+	{
+		startTime = Time.time;
+	}
+
+	void FixedUpdate()
+	{
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		if (Time.time - startTime > 5)
+			Destroy (gameObject);
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.transform.tag == "Enemy") 
+		{
+			col.gameObject.GetComponent<Life> ().TakeDamage (damage);
+		}
+		Destroy (gameObject);
+	}
+}
