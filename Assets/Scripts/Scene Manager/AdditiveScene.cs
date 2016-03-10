3e8832105f6b074db2e0eb1a817b6a81d470c852
Supachai Laparparat <supachailllpay@gmail.com>
2016-03-10 17:43:11 +0700

    Add AdditiveScene.cs

---
+++ Assets/Scripts/Scene Manager/AdditiveScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AdditiveScene : MonoBehaviour {
+
+	public string name;
+
+	void OnTriggerEnter (Collider other) {
+		if (!SceneManager.GetSceneByName (name).isLoaded)
+			StartCoroutine (LoadAdditiveScene ());
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (SceneManager.GetSceneByName (name).isLoaded)
+			StartCoroutine (UnLoadAdditiveScene ());
+	}
+
+	IEnumerator LoadAdditiveScene () {
+		yield return new WaitForSeconds (0.1f);
+		SceneManager.LoadScene (name, LoadSceneMode.Additive);
+	}
+
+	IEnumerator UnLoadAdditiveScene () {
+		yield return new WaitForSeconds (0.1f);
+		SceneManager.UnloadScene (name);
+	}
+}
