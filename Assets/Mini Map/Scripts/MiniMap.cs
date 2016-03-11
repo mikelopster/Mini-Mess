59c50b73dd6d30cb311439f821f7ab56bd0b5bed
Supachai Laparparat <supachailllpay@gmail.com>
2016-03-11 09:14:44 +0700

    Add Mini Map to scene

--- Assets/Mini Map/Scripts/MiniMap.cs
+++ Assets/Mini Map/Scripts/MiniMap.cs
@@ -5,6 +5,10 @@ public class MiniMap : MonoBehaviour {
 
 	public GameObject map;
 
+	void Start () {
+		map.SetActive (false);
+	}
+
 	void Update () {
 		if (Input.GetButtonDown ("Mini Map")) {
 			bool active = map.activeInHierarchy;
