fb7fb2b463bf41d8fbfe736754363e7891dbfe5d
Supachai Laparparat <supachailllpay@gmail.com>
2016-03-11 08:40:41 +0700

    Add Mini Map feature

---
+++ Assets/Mini Map/Scripts/MiniMap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+
+	public GameObject map;
+
+	void Update () {
+		if (Input.GetButtonDown ("Mini Map")) {
+			bool active = map.activeInHierarchy;
+			map.SetActive (!active);
+		}
+	}
+}
---
+++ Assets/Mini Map/Scripts/MiniMapCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapCamera : MonoBehaviour {
+
+	public Transform head;
+	public Transform body;
+
+	Vector3 position;
+	Vector3 rotation;
+
+	void Awake () {
+		position = new Vector3 ();
+		rotation = new Vector3 ();
+	}
+
+	void Update () {
+		position.Set (head.position.x, transform.position.y, head.position.z);
+		rotation.Set (transform.eulerAngles.x, body.eulerAngles.y, transform.eulerAngles.z);
+
+		transform.position = position;
+		transform.eulerAngles = rotation;
+	}
+}
