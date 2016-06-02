239e7c3ee51fbd88a956c87e221c567634ac67a2
Tanitphon Paniwan <tanit.pani@gmail.com>
2016-06-02 18:11:26 +0700

    Bug Fixed

---
+++ Assets/Scripts/MovementSync.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementSync : MonoBehaviour {
+
+
+	public Transform endMarker;
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		journeyLength = Vector3.Distance(transform.position, endMarker.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp(transform.position, endMarker.position, fracJourney);
+	}
+
+	void MoveSync() {
+
+	}
+}
